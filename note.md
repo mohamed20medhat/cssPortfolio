86ab833b74ec0f1b90bf07a74b28a736f9c72e1b
mohamed20medhat <egailmohamed2018@gmail.com>
2022-09-17 13:06:40 +0200

    finished project

---
+++ note.md
@@ -0,0 +1,5 @@
+# topics 
+- [Build a Responsive Online Portfolio from scratch!](https://youtu.be/-D6oTPA4vXc) 1:00:29
+- [online photo editor](https://pixlr.com/)
+- 
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const hamburgerBtn = document.getElementById('hamburger')
+const navList = document.getElementById("nav-list")
+
+function toggleButton() {
+  navList.classList.toggle('show')
+}
+
+hamburgerBtn.addEventListener('click', toggleButton)
+
+
+
+
+
+
+
